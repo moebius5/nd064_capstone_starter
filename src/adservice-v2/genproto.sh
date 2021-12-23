59c62e9dd788c4aa8a8cdc0747e3b22b6fb08b9c
moebius5 <muhamed.berdimuradov@gmail.com>
2021-12-23 14:01:40 +0200

    modified:   genproto.sh

--- src/adservice-v2/genproto.sh
+++ src/adservice-v2/genproto.sh
@@ -3,4 +3,4 @@
 set -e
 
 # TODO: Add the commands to generate the gRPC files
-python -m grpc_tools.protoc -I../../pb --python_out=./ --grpc_python_out=./ ../../pb/demo.proto
+python3 -m grpc_tools.protoc -I../../pb --python_out=./ --grpc_python_out=./ ../../pb/demo.proto
